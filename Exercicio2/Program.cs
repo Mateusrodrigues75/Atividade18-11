458b696ee99ecf8964b0d474e22870bd5a386545
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-18 16:49:08 -0300

    Finalizado

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password;
+            Console.WriteLine("Digite um nome de usuário: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Digite uma senha: ");
+            password = Console.ReadLine();
+
+            while(password == username){
+                Console.WriteLine(" ----------------ERRO------------------ \nSenha igual ao nome de usuário. \nDigite outra senha : ");
+                password = Console.ReadLine();
+            }
+            Console.WriteLine("Senha válida!");
+            Console.WriteLine("FIM");
+
+        }
+    }
+}
