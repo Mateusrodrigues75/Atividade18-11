1cc69d89e23e85bf84082aa4faf5c7c62af27800
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-18 16:33:44 -0300

    Finalizado

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num;
+            Console.WriteLine("Digite uma note entre 0 e 10: ");
+            num = decimal.Parse(Console.ReadLine());
+
+            while(num<0 || num>10){
+                
+                Console.WriteLine("Nota Inválida");
+                Console.WriteLine("Digite uma note entre 0 e 10: ");
+                num = decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"A nota digitada foi {num}");
+            Console.WriteLine("FIM");
+
+        }
+    }
+}
