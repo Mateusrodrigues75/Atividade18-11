3cc9f616760a52f728e13fc389a2836c3c669e5a
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-18 23:03:43 -0300

    Finalizado

---
+++ Desafio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroAnt, numeroAtual, fibonacci;
+            numeroAnt = 0;
+            numeroAtual = 1;
+
+            for(int i = 0; i < 14; i++){
+                fibonacci = numeroAnt + numeroAtual;
+                Console.WriteLine (fibonacci);
+                numeroAnt = numeroAtual;
+                numeroAtual = fibonacci;
+            }
+            
+        }
+    }
+}
